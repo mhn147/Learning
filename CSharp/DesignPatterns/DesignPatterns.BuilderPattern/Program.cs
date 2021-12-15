ece5ca828014b9c76c556dcf7c1f1935bf15a5cc
mhn147 <muhammed.al.nasri@gmail.com>
2021-12-15 12:23:47 +0100

    Made the Builder fluent

--- CSharp/DesignPatterns/DesignPatterns.BuilderPattern/Program.cs
+++ CSharp/DesignPatterns/DesignPatterns.BuilderPattern/Program.cs
@@ -32,8 +32,10 @@ public class Program
         Console.WriteLine("****");
 
         var builder = new HtmlBuilder("ul");
-        builder.AddChild("li", "hello");
-        builder.AddChild("li", "world");
+
+        builder.AddChild("li", "hello")
+               .AddChild("li", "world");
+        
         Console.WriteLine(builder);
     }
 }
