31fb16ba4e4444957d56847b021afa155b541052
mhn147 <muhammed.al.nasri@gmail.com>
2021-12-15 12:07:49 +0100

    Life without Builder

--- CSharp/DesignPatterns/DesignPatterns.BuilderPattern/Program.cs
+++ CSharp/DesignPatterns/DesignPatterns.BuilderPattern/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+namespace DesignPatterns.BuilderPattern;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Too much low level details here
+        // Higher level API is needed
+        var hello = "Hello";
+
+        var sb = new StringBuilder();
+
+        sb.Append("<p>");
+        sb.Append(hello);
+        sb.Append("</p>");
+
+        Console.WriteLine(sb);
+
+        var words = new[] { "Hello", "World" };
+        sb.Clear();
+        sb.Append("<ul>");
+        foreach (var word in words)
+        {
+            sb.Append($"<li>{word}</li>");
+        }
+        sb.Append("</ul>");
+
+        Console.WriteLine(sb);
+    }
+}
